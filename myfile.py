5cdbf5afb124a066408e52d5fbfc6ae45f71d60e
Meet Patel <159328007+mpatel029@users.noreply.github.com>
2024-08-29 14:41:28 -0400

    Added from the Previous Repo

---
+++ myfile.py
@@ -0,0 +1,10 @@
+def multiply(x, y):
+    return x * y
+
+result = multiply(5, 10)
+print("The result is: " + str(result))  
+
+def print_message():
+    print("This is a message.")
+
+print_message()
