d634b0079eb1212e75e8b8e560c81502e4f75359
Meet Patel <159328007+mpatel029@users.noreply.github.com>
2024-09-13 01:53:02 +0000

    Updated test_2 to pass

--- tests/test_2.py
+++ tests/test_2.py
@@ -4,4 +4,4 @@ sys.path.append((os.path.join(os.path.dirname(__file__), '..')))
 from myfile import multiply
 
 def test_multiply_failure():
-    assert multiply(2, 3) == 7  # This should fail
+    assert multiply(2, 3) == 6  
