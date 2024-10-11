eea6f8a1c23df563ea2516a9dcd971e67d894fba
Anchita Ramani <83870242+Anchita15@users.noreply.github.com>
2024-10-11 07:12:51 +0530

    Task 3

---
+++ Hw4/task3.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+awk -F',' 'BEGIN {OFS=","} $3 == 2 && $NF ~ /S/ {print $0}' titanic.csv | sed 's/female/F/g; s/male/M/g' |  awk -F',' 'BEGIN {OFS=","} {print $0} {if($7 != "") {sum += $7; count++}} END {if(count > 0) Avg_age =sum/count; print "Average Age : " Avg_age}'
